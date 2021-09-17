464ef8bca8fb1b693895166d671353e600dfcd1b
Varun Krishna Pappusetti <vpkrishna99@gmail.com>
2021-09-16 19:00:34 -0700

    Created "CreateSubAccounts" section

---
+++ app/src/main/java/com/example/momoneynoproblem/CreateSubAccounts.java
@@ -0,0 +1,14 @@
+package com.example.momoneynoproblem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateSubAccounts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_sub_accounts2);
+    }
+}
