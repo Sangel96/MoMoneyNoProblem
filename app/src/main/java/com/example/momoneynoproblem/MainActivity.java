c7a795117a13638f695f9f4406ffa8b771796b61
tag1973 <taghreedalshannaq@gmail.com>
2021-09-21 10:19:14 -0400

    implement Add transaction.java

--- app/src/main/java/com/example/momoneynoproblem/MainActivity.java
+++ app/src/main/java/com/example/momoneynoproblem/MainActivity.java
@@ -2,28 +2,57 @@ package com.example.momoneynoproblem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.momoneynoproblem.Login.Login;
-import com.example.momoneynoproblem.UserSignUp.UserSignUp;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     private Button btnLogout;
+//    private ArrayList<SourceType>mSourceList;
+//    private SourceAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        initList();
 
-        btnLogout = findViewById(R.id.logout);
+//        Spinner spinnerSources= findViewById(R.id.spinner);
+//        mAdapter = new SourceAdapter(this,mSourceList);
+
+//        spinnerSources.setAdapter(mAdapter);
+//        spinnerSources.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//                                                     @Override
+//                                                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                                                         SourceType ClickedItem = (SourceType) parent.getItemAtPosition(position);
+//                                                         String clickedSourceType = ClickedItem.getsourceName();
+//                                                         Toast.makeText(MainActivity.this,clickedSourceType+ " selected",
+//                                                                 Toast.LENGTH_SHORT).show();
+
+//                                                     }
+
+//                                                     @Override
+//                                                     public void onNothingSelected(AdapterView<?> parent) {
+
+//                                                     }
+//                                                 });
+
+
+                btnLogout = findViewById(R.id.logout);
         mAuth = FirebaseAuth.getInstance();
 
         btnLogout.setOnClickListener(view -> {
@@ -31,6 +60,15 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, Login.class));
         });
     }
+//    private void initList(){
+//        mSourceList= new ArrayList<>();
+
+//        mSourceList.add(new SourceType("Salary"));
+//        mSourceList.add(new SourceType("Rent"));
+//        mSourceList.add(new SourceType("Clothes"));
+//        mSourceList.add(new SourceType("Travel"));
+//        mSourceList.add(new SourceType("Food"));
+//    }
 
     @Override
     protected void onStart() {
@@ -41,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-    //register = (TextView) findViewbyId(R.id.register);
+    //register = (TextView) findViewId(R.id.register);
     //register.setOnClickListener(this);
 
 
