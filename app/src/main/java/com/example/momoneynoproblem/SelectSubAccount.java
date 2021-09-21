59f7411ca17d81a9e8a9f5ddf7f6b08f22a5768a
Varun Krishna Pappusetti <vpkrishna99@gmail.com>
2021-09-20 19:57:09 -0700

    SubAccount and SelectAccount

---
+++ app/src/main/java/com/example/momoneynoproblem/SelectSubAccount.java
@@ -0,0 +1,14 @@
+package com.example.momoneynoproblem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectSubAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_sub_account);
+    }
+}
