1276eb6f61ea3a450c909c13da83dd35d27b5118
Huan <huando12@gmail.com>
2021-10-12 20:30:07 -0700

    Cleaned up MainActivity.java

--- app/src/main/java/com/example/momoneynoproblem/MainActivity.java
+++ app/src/main/java/com/example/momoneynoproblem/MainActivity.java
@@ -22,37 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     private Button btnLogout;
-//    private ArrayList<SourceType>mSourceList;
-//    private SourceAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        initList();
 
-//        Spinner spinnerSources= findViewById(R.id.spinner);
-//        mAdapter = new SourceAdapter(this,mSourceList);
-
-//        spinnerSources.setAdapter(mAdapter);
-//        spinnerSources.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//                                                     @Override
-//                                                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                                                         SourceType ClickedItem = (SourceType) parent.getItemAtPosition(position);
-//                                                         String clickedSourceType = ClickedItem.getsourceName();
-//                                                         Toast.makeText(MainActivity.this,clickedSourceType+ " selected",
-//                                                                 Toast.LENGTH_SHORT).show();
-
-//                                                     }
-
-//                                                     @Override
-//                                                     public void onNothingSelected(AdapterView<?> parent) {
-
-//                                                     }
-//                                                 });
-
-
-                btnLogout = findViewById(R.id.logout);
+        btnLogout = findViewById(R.id.logout);
         mAuth = FirebaseAuth.getInstance();
 
         btnLogout.setOnClickListener(view -> {
@@ -60,15 +36,6 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, Login.class));
         });
     }
-//    private void initList(){
-//        mSourceList= new ArrayList<>();
-
-//        mSourceList.add(new SourceType("Salary"));
-//        mSourceList.add(new SourceType("Rent"));
-//        mSourceList.add(new SourceType("Clothes"));
-//        mSourceList.add(new SourceType("Travel"));
-//        mSourceList.add(new SourceType("Food"));
-//    }
 
     @Override
     protected void onStart() {
@@ -78,9 +45,4 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, Login.class));
     }
 }
-
-    //register = (TextView) findViewId(R.id.register);
-    //register.setOnClickListener(this);
-
-
 }
