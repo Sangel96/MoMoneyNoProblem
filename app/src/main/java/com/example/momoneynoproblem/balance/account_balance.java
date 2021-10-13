670fd0c519b0ede6a51925a0103021cd6adf16db
sam <sam.mata@student.csulb.edu>
2021-10-13 02:32:05 -0700

    Sam - Task 11 and 17 (Account Balance and Account Report)

---
+++ app/src/main/java/com/example/momoneynoproblem/balance/account_balance.java
@@ -0,0 +1,53 @@
+package com.example.momoneynoproblem.balance;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.momoneynoproblem.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class account_balance extends AppCompatActivity {
+
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_balance);
+        listView = (ListView) findViewById(R.id.listView);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        ArrayList<String> Account_Balance = new ArrayList<>();
+        ArrayAdapter<String> ArrayAdapter = new ArrayAdapter<String>(account_balance.this, android.R.layout.simple_list_item_1, Account_Balance);
+        listView.setAdapter(ArrayAdapter);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Account_Balance.clear();
+                for (DataSnapshot temp : snapshot.getChildren()){
+                    Account_Balance.add("Account Balance: " + temp.getValue().toString());
+
+                }
+                ArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+    }
+}
