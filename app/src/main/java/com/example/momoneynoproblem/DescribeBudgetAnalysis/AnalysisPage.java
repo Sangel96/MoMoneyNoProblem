355ccfa33ba9193841c6aedbd7cb139ede27711c
VarunKrishnaPappusetti1501 <vpkrishna99@gmail.com>
2021-10-19 16:24:57 -0700

    Finished Ask Symptoms Page Unrefined and got Data

--- app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/AnalysisPage.java
+++ app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/AnalysisPage.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.momoneynoproblem.R;
 
-public class AnalysisPage extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class AnalysisPage extends AppCompatActivity {
+    SeekBar seekbar;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis_page);
 
         // SeekBar Chhange
-        Seekbar
+        seekbar = (SeekBar) findViewById(R.id.seekBar);
+        textView = (TextView) findViewById(R.id.newBudget) ;
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
