acf9678fc656c5e356d6e62777ea64f989d64759
VarunKrishnaPappusetti1501 <vpkrishna99@gmail.com>
2021-10-19 00:07:14 -0700

    Created EmergencyButton

---
+++ app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/EmergencyButton.java
@@ -0,0 +1,14 @@
+package com.example.momoneynoproblem.DescribeBudgetAnalysis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmergencyButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_button);
+    }
+}
