9802c9e72de87fb4e164f72129a874d68ec0de2b
VarunKrishnaPappusetti1501 <vpkrishna99@gmail.com>
2021-10-19 09:01:11 -0700

    Connected Ask Symptoms Page to Analysis Page

--- app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/AskSymptoms.java
+++ app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/AskSymptoms.java
@@ -2,7 +2,9 @@ package com.example.momoneynoproblem.DescribeBudgetAnalysis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.momoneynoproblem.R;
 
@@ -12,5 +14,9 @@ public class AskSymptoms extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_symptoms);
+        Button enterSymptomButton = findViewById(R.id.EnterButton);
+        enterSymptomButton.setOnClickListener(view -> {
+            startActivity(new Intent(this,AnalysisPage.class));
+        });
     }
 }
