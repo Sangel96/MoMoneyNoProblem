3bbe1969efd25c86ae4232f49c4f578f14943298
VarunKrishnaPappusetti1501 <vpkrishna99@gmail.com>
2021-10-19 01:45:32 -0700

    Completed and created AnalysisPage

---
+++ app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/AnalysisPage.java
@@ -0,0 +1,16 @@
+package com.example.momoneynoproblem.DescribeBudgetAnalysis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.momoneynoproblem.R;
+
+public class AnalysisPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis_page);
+    }
+}
