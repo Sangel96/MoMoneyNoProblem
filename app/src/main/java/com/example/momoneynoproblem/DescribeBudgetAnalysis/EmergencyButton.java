3b4a0d61dd6a99a02d75e9443be33e139c3cdf5d
VarunKrishnaPappusetti1501 <vpkrishna99@gmail.com>
2021-10-19 08:24:46 -0700

    Connected Emergency Button page to AskSymptoms Page and updated the AndroidManifest.xml to set the default activity to show up as is the Login Page.

--- app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/EmergencyButton.java
+++ app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/EmergencyButton.java
@@ -2,7 +2,9 @@ package com.example.momoneynoproblem.DescribeBudgetAnalysis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.momoneynoproblem.R;
 
@@ -12,5 +14,11 @@ public class EmergencyButton extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_button);
+
+        Button emergency_button = findViewById(R.id.EmergencyButtonID); //Initializes Emergency Button
+        emergency_button.setOnClickListener(view -> {
+            startActivity(new Intent(this,AskSymptoms.class));
+        });
+
     }
 }
