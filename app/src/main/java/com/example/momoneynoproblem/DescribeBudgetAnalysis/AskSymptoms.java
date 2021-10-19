dd6f2fc7e29abacabde933e46c37770c1f78c1ae
VarunKrishnaPappusetti1501 <vpkrishna99@gmail.com>
2021-10-19 01:19:22 -0700

    Created AskSymptoms XML

---
+++ app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/AskSymptoms.java
@@ -0,0 +1,16 @@
+package com.example.momoneynoproblem.DescribeBudgetAnalysis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.momoneynoproblem.R;
+
+public class AskSymptoms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_symptoms);
+    }
+}
