9f206917ee1ceb5dc814e2d7364112b1d867146a
VarunKrishnaPappusetti1501 <vpkrishna99@gmail.com>
2021-10-19 01:13:58 -0700

    Created EmergencyButton XML Page

--- app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/EmergencyButton.java
+++ app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/EmergencyButton.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.momoneynoproblem.R;
+
 public class EmergencyButton extends AppCompatActivity {
 
     @Override
