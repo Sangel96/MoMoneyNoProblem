ca125b0498234865962a1639a09fcfae6394b7f8
VarunKrishnaPappusetti1501 <vpkrishna99@gmail.com>
2021-10-31 19:28:42 -0700

    Finished Sub Account UI

---
+++ app/src/main/java/com/example/momoneynoproblem/SubAccount/sub_accounts.java
@@ -0,0 +1,16 @@
+package com.example.momoneynoproblem.SubAccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.momoneynoproblem.R;
+
+public class sub_accounts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_accounts);
+    }
+}
