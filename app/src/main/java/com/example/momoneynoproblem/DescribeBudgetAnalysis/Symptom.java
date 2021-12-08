bedc95fd2a1b31431cdbbe1edc2bf4fe97db0e74
VarunKrishnaPappusetti1501 <vpkrishna99@gmail.com>
2021-12-07 17:08:00 -0800

    Ask Symptoms Page updated. UI sometimes work with constraint.

--- app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/Symptom.java
+++ app/src/main/java/com/example/momoneynoproblem/DescribeBudgetAnalysis/Symptom.java
@@ -2,18 +2,20 @@ package com.example.momoneynoproblem.DescribeBudgetAnalysis;
 
 import android.widget.DatePicker;
 
+import java.util.Date;
+
 public class Symptom {
 
     public String radioButtonSelection;
-//    public DatePicker mdate;
+    public String mdate;
     public String emergLevelSelection;
 
     public Symptom() {
     }
 
-    public Symptom(String radioButtonSelection, String emergLevelSelection) {
+    public Symptom(String radioButtonSelection, String emergLevelSelection, String mdate) {
         this.radioButtonSelection = radioButtonSelection;
-//        this.mdate = mdate;
+        this.mdate = mdate;
         this.emergLevelSelection = emergLevelSelection;
     }
 }
